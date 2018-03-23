16d1c7dc6bd3a31546671843aefff5b9b1723822
AnhNH (VN_LTT) <anhnh@lifetimetech.vn>
2018-03-23 10:43:47 +0700

    border textfield

--- EVNCustomSearchBar/EVNCustomSearchBar.m
+++ EVNCustomSearchBar/EVNCustomSearchBar.m
@@ -107,7 +107,7 @@
         [_textField addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
         _textField.autoresizingMask = UIViewAutoresizingFlexibleWidth;
         _textField.borderStyle = UITextBorderStyleNone;
-        _textField.layer.cornerRadius = 3.0f;
+        _textField.layer.cornerRadius = 15.0f;
         _textField.layer.masksToBounds = YES;
         _textField.leftView.contentMode = UIViewContentModeScaleAspectFit;
         _textField.backgroundColor = [UIColor colorWithRed:240.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0];
